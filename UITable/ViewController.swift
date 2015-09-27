a4509117994b30e9315065f3d856a4b7ba056841
yash2710 <13bce123@nirmauni.ac.in>
2015-09-27 17:33:17 +0530

    final

--- UITable/ViewController.swift
+++ UITable/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier",
+        "Bourke Street Bakery", "Haigh’s Chocolate", "Palomino Espresso",
+        "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina",
+        "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return restaurantNames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+                let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+                cell.textLabel!.text = restaurantNames[indexPath.row]
+                cell.imageView?.image = UIImage(named: "restaurant")
+                return cell
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+                    return true
+    }
 
 }
 
